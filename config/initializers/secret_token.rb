936364f9a4181e3c7e63187f823d2a25747f4f23
Kuan Chen <immachink@gmail.com>
2012-10-06 16:10:10 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Educomer::Application.config.secret_token = '467f26357245fe816e92afcfeb4778d5f23b540c951a7774dd49a0fdf534c55d00e9048ee0a8ad916965ef5263d16f8666d0975a0f1e4ab30de96307c880ff6d'
