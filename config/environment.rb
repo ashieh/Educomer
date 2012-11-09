c09da004eac0d8e0fec87cc24eba1626ea5cc882
Stephen Li <venturanima@gmail.com>
2012-11-09 01:06:15 -0800

    encoding default set to iso-8859-1

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Educomer::Application.initialize!
 
-#~ Encoding.default_external = Encoding::ISO-8859-1
-#~ Encoding.default_internal = Encoding::iso-8859-1
+Encoding.default_external = Encoding::ISO-8859-1
+Encoding.default_internal = Encoding::iso-8859-1
